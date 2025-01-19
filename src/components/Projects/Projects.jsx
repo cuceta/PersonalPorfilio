323178dfbe86cfcf82cc094bcf616c1ce28277d7
cuceta <cuceta@oswego.edu>
2025-01-19 17:23:13 -0500

    Family Thread project page done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,30 +3,22 @@ import './Projects.css';
 import familyThreadThumbnail from '../../assets/projects/familyThread/thumbnail.png';
 import placeholderThumbnail from '../../assets/projects/placeholderthumbnail.jpg';
 import closeSymbol from '../../assets/closeSymbol.svg'
-import projectPage from '../ProjectPages/projectPage.jsx'
 import ProjectPage from "../ProjectPages/projectPage.jsx";
+import FamilyThread from '../ProjectPages/FamilyThread/FamilyThread.jsx'
 
 const projects = [
     {
         id: 1,
         title: 'Family Thread Web Application',
         description: 'A minimum viable product for a family tree application.',
-        details: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Urna quis pretium ultrices laoreet quisque suscipit. Nascetur ex tempor fringilla consectetur potenti ullamcorper turpis amet ultricies. Mi morbi faucibus malesuada habitasse eget eleifend. Mus condimentum eget ac mauris; accumsan eleifend. Litora in etiam varius diam posuere feugiat blandit viverra purus. Nulla cras tortor adipiscing; dui risus tempus dictum id.\n' +
-            '\n\n' +
-            'Euismod scelerisque habitasse finibus, purus aenean curabitur. Suscipit sapien risus sem velit pellentesque. Lectus tincidunt suspendisse tortor curae ad platea vel. Risus at dapibus tincidunt morbi scelerisque. Odio magnis pulvinar montes, senectus phasellus pharetra fringilla imperdiet. Bibendum consectetur montes aptent quam auctor proin phasellus. At etiam netus volutpat molestie libero lacinia aptent egestas sit.\n' +
-            '\n\n' +
-            'Consectetur natoque nec justo primis nisl nullam? Sociosqu curabitur nec eleifend consectetur et ac. Condimentum sagittis quam quis porttitor elit. Nunc non odio nunc ex erat vehicula pulvinar. Aenean facilisis eu proin cubilia iaculis iaculis est turpis. Adipiscing purus orci malesuada maximus vivamus turpis ac sociosqu laoreet. Accumsan ad pretium natoque potenti lacinia class.\n' +
-            '\n\n' +
-            'Aliquet euismod felis ultricies sollicitudin lectus maecenas himenaeos auctor. Lacus ex fermentum volutpat imperdiet sem rhoncus egestas velit. Maecenas sagittis dignissim facilisi pulvinar venenatis. Senectus lacinia justo eget pulvinar ornare tellus. Felis fames dui gravida porttitor consequat convallis scelerisque himenaeos nascetur. Ligula leo elementum tellus pretium viverra sollicitudin iaculis turpis. Parturient dictumst maecenas vulputate eget dis tempor. Eros sit arcu efficitur laoreet class velit nascetur orci. Quis conubia ornare sit dictum, quam bibendum morbi.\n' +
-            '\n\n' +
-            'Scelerisque vel dignissim himenaeos litora, duis lectus. Parturient tincidunt scelerisque tincidunt auctor leo ipsum; fusce amet vivamus. Ullamcorper ac tristique elementum iaculis massa id volutpat. Dapibus ac risus congue habitant montes faucibus. Inceptos risus nec ornare condimentum, bibendum enim lectus aenean. Vulputate mauris quam turpis, urna quam nascetur. Metus curae iaculis euismod integer convallis.',
+        details: <FamilyThread/>,
         thumbnail: familyThreadThumbnail,
     },
     {
         id: 2,
         title: 'CheckMate To-Do List Web Application',
         description: 'My personal portfolio to showcase my work.',
-        details: <ProjectPage></ProjectPage>,
+        details: <ProjectPage/>,
         thumbnail: placeholderThumbnail,
     },
     {
