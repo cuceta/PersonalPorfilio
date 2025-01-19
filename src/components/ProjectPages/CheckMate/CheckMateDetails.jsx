eb0e8c739ccb420e850792418953cf0ffdf7e5da
cuceta <cuceta@oswego.edu>
2025-01-19 17:33:31 -0500

    Project pages linked

--- src/components/ProjectPages/CheckMate/CheckMateDetails.jsx
+++ src/components/ProjectPages/CheckMate/CheckMateDetails.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './CheckMateDetails.css'
 
 const CheckMateDetails = () => {
     return (
-        <div>
-
+        <div className="check-mate">
+            <h3>Project Goal:</h3>
+            <p>
+                Develop a fully functional web application with the capability to allow users to create, save
+                and share their family trees as an introductory software engineering project.
+            </p>
+            <h3>Involvement:</h3>
+            <p>
+                <ul>
+                    <li>Front-End Development</li>
+                    <li>UX/UI Design</li>
+                    <li>Communication with stakeholders and potential clients</li>
+                </ul>
+            </p>
         </div>
     );
 };
