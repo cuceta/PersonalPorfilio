eeb2fbe2947b14ab2cadd517e17a3de14c2432e6
cuceta <cuceta@oswego.edu>
2025-01-27 10:52:53 -0500

    Contact me form started

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import AboutMe from "./components/Aboutme/aboutMe.jsx";
 import Projects from "./components/Projects/Projects.jsx";
 import Experience from "./components/Experience/Experience.jsx";
+import ContactMe from "./components/ContactMe/ContactMe.jsx";
 
 const App = () => {
     return(
@@ -13,6 +14,7 @@ const App = () => {
             <Projects/>
             <Experience/>
             <Projects/>
+            <ContactMe/>
         </div>
 
     )
