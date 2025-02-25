3615006302059c2b3ee36e0afb990031065f33c1
cuceta <cuceta@oswego.edu>
2025-02-25 09:00:16 -0500

    formatting

--- src/components/Aboutme/aboutMe.jsx
+++ src/components/Aboutme/aboutMe.jsx
@@ -6,56 +6,60 @@ import github from '../../assets/github.svg';
 import linkedin from '../../assets/linkedin.svg';
 import email from '../../assets/gmail.svg';
 import CV from '../../assets/CrislennyUcetaResume.pdf';
+import downArrow from '../../assets/down_arrow.png'
 
 const AboutMe = () => {
     return (
         <section className="about-me" id='about'>
-            <div className="content-wrapper">
-                <div className="profile-container">
-                    <div className="oval-background"></div>
-                    <img src={profile} alt="Illustration of Crislenny Uceta" className="profile-image" />
-                </div>
-                <div className="text-container">
-                    <h3>Hello there! It’s me, Crislenny!</h3>
-                    <h2>
-                        <Typewriter
-                            options={{
-                                strings: [
-                                    "I'm a Front-end Developer",
-                                    "I'm a Graphic Designer",
-                                    "I'm a Web Developer",
-                                    "I'm a UX/UI Designer"
-                                ],
-                                autoStart: true,
-                                loop: true,
-                                cursor: '|',
-                            }}
-                        />
-                    </h2>
-                    <p>
-                        I am passionate about designing in a timeless manner in a field
-                        where I can merge technology and design. Therefore, I am an
-                        aspiring web developer who strives to learn and get better every day.
-                    </p>
-                    <div className="social">
-                        <a href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedin} alt="LinkedIn logo" className="social-icon"/>
-                        </a>
-                        <a href="mailto:cuceta@oswego.edu">
-                            <img src={email} alt="Email logo" className="social-icon"/>
-                        </a>
-                        <a href="https://github.com/cuceta" target="_blank" rel="noopener noreferrer">
-                            <img src={github} alt="Email logo" className="social-icon"/>
-                        </a>
+            <div className="main-div">
+                <div className="content-wrapper">
+                    <div className="profile-container">
+                        <div className="oval-background"></div>
+                        <img src={profile} alt="Illustration of Crislenny Uceta" className="profile-image"/>
                     </div>
-                    <div>
-                        <a href={CV} download>
-                            <button className="resume-button">
-                                Resume
-                            </button>
-                        </a>
+                    <div className="text-container">
+                        <h3>Hello there! It’s me, Crislenny!</h3>
+                        <h2>
+                            <Typewriter
+                                options={{
+                                    strings: [
+                                        "I'm a Front-end Developer",
+                                        "I'm a Graphic Designer",
+                                        "I'm a Web Developer",
+                                        "I'm a UX/UI Designer"
+                                    ],
+                                    autoStart: true,
+                                    loop: true,
+                                    cursor: '|',
+                                }}
+                            />
+                        </h2>
+                        <p>
+                            I am passionate about designing in a timeless manner in a field
+                            where I can merge technology and design. Therefore, I am an
+                            aspiring web developer who strives to learn and get better every day.
+                        </p>
+                        <div className="social">
+                            <a href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">
+                                <img src={linkedin} alt="LinkedIn logo" className="social-icon"/>
+                            </a>
+                            <a href="mailto:cuceta@oswego.edu">
+                                <img src={email} alt="Email logo" className="social-icon"/>
+                            </a>
+                            <a href="https://github.com/cuceta" target="_blank" rel="noopener noreferrer">
+                                <img src={github} alt="Email logo" className="social-icon"/>
+                            </a>
+                        </div>
+                        <div>
+                            <a href={CV} download>
+                                <button className="resume-button">
+                                    Resume
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 </div>
+                <img src={downArrow} alt="Arrow pointing down"/>
             </div>
         </section>
     );
